a59aa3775007ea3e439b14a8f8d822ee27a79207
salma mojica <salmamojica24@gmail.com>
2026-03-10 08:11:32 -0500

    se le agregaron varios RegEx a los campos de registro

--- js/script.js
+++ js/script.js
@@ -11,8 +11,44 @@ function solicitarServicio() {
 
 function registrar() {
 
-    let nombre = document.getElementById("nombre").value
-    let correo = document.getElementById("correo").value
+
+    var nombre = document.getElementById("nombre").value
+    let nombreRegex = /^[a-zA-Z\s]+$/;
+
+    if (!nombreRegex.test(nombre)) {
+        alert("Nombre no válido. Solo se permiten letras y espacios.");
+        return; 
+    }
+
+    var apellidos = document.getElementById("apellidos").value
+    let apellidosRegex = /^[a-zA-Z\s]+$/;
+
+    if (!apellidosRegex.test(apellidos)) {
+        alert("Apellidos no válidos. Solo se permiten letras y espacios.");
+        return; 
+    }
+
+    var edad = document.getElementById("edad").value
+    let edadRegex = /^\d+$/;
+
+    if (!edadRegex.test(edad) || edad < 0 || edad > 120) {
+        alert("Edad no válida. Debe ser un número entre 0 y 120.");
+        return; 
+    }
+
+
+
+    var telefono = document.getElementById("telefono").value
+    let telefonoRegex = /^\d{10}$/;
+
+    if (!telefonoRegex.test(telefono)) {
+        alert("Número de teléfono no válido. Debe contener 10 dígitos.");
+        return;// Detener la ejecución si el número de teléfono no es válido
+    }
+
+
+
+
 
     let regex = /@/
 
